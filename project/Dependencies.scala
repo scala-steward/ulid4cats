91f8b0d63f882bea8f9b2d63128b2445cb38c32f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-14 20:07:16 +0200

    Minor/major updates (#121)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
   lazy val catsCore = Def.setting("org.typelevel" %%% "cats-core" % "2.13.0")
-  lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.6.3")
-  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.4")
+  lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.7.0")
+  lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.3.2")
   lazy val munitCatsEffect =
-    Def.setting("org.typelevel" %%% "munit-cats-effect" % "2.1.0")
+    Def.setting("org.typelevel" %%% "munit-cats-effect" % "2.2.0")
 }
